723bc45597c32c26e074c381400bd53185fdc5ff
Meklin1 <93739199+Meklin1@users.noreply.github.com>
2024-05-12 00:04:15 +0300

    Initialized recipe service

---
+++ RecipesAPI/Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,14 @@
+﻿using RecipesAPI.Filters;
+using RecipesAPI.Models;
+
+namespace RecipesAPI.Services.Interfaces
+{
+    public interface IRecipeService
+    {
+        Task DeleteRecipeByIdAsync(long id);
+        Task<Recipe> GetRecipeByIdAsync(long id);
+        Task<Recipe> UpdateRecipeByIdAsync(Recipe body, long id);
+        Task<Recipe> CreateRecipeAsync(Recipe body);
+        Task<List<Recipe>> GetRecipesAsync(RecipeFilter filter);
+    }
+}
---
+++ RecipesAPI/Services/RecipeService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipesAPI.Filters;
+using RecipesAPI.Models;
+using RecipesAPI.Repositories.Interfaces;
+using RecipesAPI.Services.Interfaces;
+
+namespace PoS.Application.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+
+        public RecipeService(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        public Task<Recipe> CreateRecipeAsync(Recipe body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRecipeByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Recipe> GetRecipeByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Recipe>> GetRecipesAsync(RecipeFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Recipe> UpdateRecipeByIdAsync(Recipe body, long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
