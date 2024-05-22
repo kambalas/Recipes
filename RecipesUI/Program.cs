ec0a23011680e65e2c9af295b3125057022a5c1e
Motiejūnas Ugnius <motiejunasugnius@gmail.com>
2024-05-22 21:16:45 +0300

    Save current changes before revert

--- RecipesUI/Program.cs
+++ RecipesUI/Program.cs
@@ -1,10 +1,7 @@
-using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using RecipesUI.Data;
 using RecipesUI.Services;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +12,6 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddScoped(typeof(IApiService<>), typeof(ApiService<>));
 builder.Services.AddScoped<IRecipeService, RecipeService>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7087/") });
-builder.Services.AddBlazoredSessionStorage();
-builder.Services.AddScoped<CustomAuthenticationStateProviderService>();
-builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthenticationStateProviderService>());
-builder.Services.AddAuthorizationCore();
-
 
 
 builder.Services.AddLogging();
