c134d98da39eee2d602e48438a842d7debe7d0fc
MrRobot2002 <just.zubavicius@gmail.com>
2024-05-24 16:35:41 +0300

    fully fixed and finished sorting, now its working

--- RecipesUI/wwwroot/js/filter.js
+++ RecipesUI/wwwroot/js/filter.js
@@ -1,53 +1,28 @@
 window.provideSortDropdownListeners = function() {
     const dropdownButton = document.querySelector('.dropdown-button.sort-button');
     const dropdownMenu = document.querySelector('.dropdown-menu');
-    const arrowIcon = document.querySelector('.arrow-icon');
 
     dropdownButton.addEventListener('click', function () {
         if (dropdownMenu.style.display === 'block') {
             dropdownMenu.style.display = 'none';
-            // arrowIcon.style.transform = 'translateY(-50%) rotate(0)';
         } else {
             dropdownMenu.style.display = 'block';
-            // arrowIcon.style.transform = 'translateY(-50%) rotate(180deg)';
+
         }
     });
 
     document.querySelectorAll('.dropdown-item').forEach(item => {
         item.addEventListener('click', function () {
             const value = this.getAttribute('data-value');
-            sortRecipes(value);
             dropdownMenu.style.display = 'none';
-            // arrowIcon.style.transform = 'translateY(-50%) rotate(0)';
         });
     });
 
     document.addEventListener('click', function (event) {
         if (!event.target.closest('.dropdown-container')) {
             dropdownMenu.style.display = 'none';
-            // -arrowIcon.style.transform = 'translateY(-50%) rotate(0)';
         }
     });
 };
 
 
-// window.sortRecipes = {
-//
-//
-//     sortRecipes : function (order) {
-//         console.log('Sorting recipes by:', order);
-//         DotNet.invokeMethodAsync('Recipes', 'SortRecipes', order) 
-//
-//         //HERE WE WILL NEED TO DECIDE ON A CONCRETE PROJECT NAME IN ORDER TO CALL THE JS METHOD
-//
-//    
-//         .then(() => {
-//             console.log('Successfully sorted recipes by:', order);
-//         })
-//             .catch(error => {
-//                 console.error('Error sorting recipes:', error);
-//             });
-//
-//     }
-
-// };
