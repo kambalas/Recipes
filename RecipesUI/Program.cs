05b4cffdf753b770f121a26213446adbc5a4665a
MrRobot2002 <just.zubavicius@gmail.com>
2024-05-27 16:16:28 +0300

    fixing errors

--- RecipesUI/Program.cs
+++ RecipesUI/Program.cs
@@ -8,6 +8,7 @@ using RecipesUI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddMudServices();
 builder.Services.AddServerSideBlazor();
@@ -36,17 +37,20 @@ builder.Services.AddLogging();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
